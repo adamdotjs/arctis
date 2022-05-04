0dcfd80603ff269750f7322e8d7f4ea52e557923
Adam <68607953+adamdotjs@users.noreply.github.com>
2022-05-04 09:03:27 -0400

    change react.jsx demo code

--- demos/react.jsx
+++ demos/react.jsx
@@ -1,17 +1,26 @@
-import { styled } from 'styled-components';
-
-const Wrapper = styled.div`
-	background: blue;
-	padding: 4px;
-`;
-
-const PageHeader = () => {
-	return (
-		<Wrapper>
-			<h1>Let's get this MONEY</h1>
-			<p>I honestly can't believe I've never heard this before. Holy shit, it's fucking amazing.</p>
-		</Wrapper>
-	);
-};
-
-export default PageHeader;
+function CandyDispenser() {
+  const initialCandies = ['snickers', 'skittles', 'twix', 'milky way']
+  const [candies, setCandies] = React.useState(initialCandies)
+  const dispense = candy => {
+    setCandies(allCandies => allCandies.filter(c => c !== candy))
+  }
+  return (
+    <div>
+      <h1>Candy Dispenser</h1>
+      <div>
+        <div>Available Candy</div>
+        {candies.length === 0 ? (
+          <button onClick={() => setCandies(initialCandies)}>refill</button>
+        ) : (
+          <ul>
+            {candies.map(candy => (
+              <li key={candy}>
+                <button onClick={() => dispense(candy)}>grab</button> {candy}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
